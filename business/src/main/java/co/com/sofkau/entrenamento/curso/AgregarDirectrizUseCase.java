b236b419e53139782d2b31816aff4f874e6cf8f7
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-16 14:12:29 -0500

    se crea el caso de uso para agregar la directriz, se crea el comando para agregar la directriz a la mentoria

---
+++ business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizUseCase.java
@@ -0,0 +1,8 @@
+package co.com.sofkau.entrenamento.curso;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+
+public class AgregarDirectrizUseCase extends UseCase<RequestCommand<>, ResponseEvents> {
+}
