1a516d9f78644174a38b9e31ae7b45d4b5fad490
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-16 15:26:16 -0500

    completado el código del comando AgregarDirectrizAMentoria

--- business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizUseCase.java
+++ business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizUseCase.java
@@ -3,6 +3,11 @@ package co.com.sofkau.entrenamento.curso;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizAMentoria;
 
-public class AgregarDirectrizUseCase extends UseCase<RequestCommand<>, ResponseEvents> {
+public class AgregarDirectrizUseCase extends UseCase<RequestCommand<AgregarDirectrizAMentoria>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<AgregarDirectrizAMentoria> agregarDirectrizAMentoriaRequestCommand) {
+        var command = agregarDirectrizAMentoriaRequestCommand.getCommand();
+    }
 }
--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizAMentoria.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizAMentoria.java
@@ -1,4 +1,30 @@
 package co.com.sofkau.entrenamiento.curso.commands;
 
-public class AgregarDirectrizAMentoria {
+import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.entrenamiento.curso.values.Fecha;
+import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
+import co.com.sofkau.entrenamiento.curso.values.Nombre;
+
+public class AgregarDirectrizAMentoria extends Command {
+    private final MentoriaId entityId;
+    private final Nombre nombre;
+    private final Fecha fecha;
+
+    public AgregarDirectrizAMentoria(MentoriaId entityId, Nombre nombre, Fecha fecha) {
+        this.entityId = entityId;
+        this.nombre = nombre;
+        this.fecha = fecha;
+    }
+
+    public MentoriaId getEntityId() {
+        return entityId;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Fecha getFecha() {
+        return fecha;
+    }
 }
